6563323d24f826044995efdf9e775d28df74279a
EKaye <holmanelisek@gmail.com>
2019-09-10 23:31:18 -0400

    psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+//timer
+//update inner html of timer
+//array of correct answers
+//empty array of user answers
+//variable correct = 0
+//variable incorrect = 0
+//variable answered = 0
+//if timer isn't 0
+//on button press
+//put the answer into that position in the user answer array
+//update all button images to be empty update that button image to be clicked
+// answered +1;
+//if answered = 5
+//stop timer
+//for (i=0;i++;i<correctAnswers.length){
+//if(correctAnswers[i]===userAnswers[i]){
+//correct+1;
+//see video, possibly wipe buttons and display correct!
+//} else{
+//incorrect+1;
+//see video, possible wipe buttons and display incorrect!
+//}}
+//
+//see video, possible wipe timer and display Correct: correct Incorrect: incorrect
+//
+//else
+//clear the buttons and display "Time's Up!" check video
